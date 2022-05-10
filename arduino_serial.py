9bc4660d42ef9691ef1b59771a26e379a74e5fbf
FedeJack06 <105308416+FedeJack06@users.noreply.github.com>
2022-05-10 20:41:29 +0200

    Add files via upload

---
+++ arduino_serial.py
@@ -0,0 +1,92 @@
+import serial
+import time
+import ROOT
+import numpy as np
+
+#begin serial
+ser = serial.Serial('/dev/ttyACM0', 9600)
+time.sleep(1)
+ser.close()
+time.sleep(1)
+ser.open()
+stato = 0
+s =1
+## PREOCESSING
+I = 0
+
+mediaVarduino = 0
+mediaVhall = 0
+devStdVard = 0
+devStdVh = 0
+vArdArray = np.array([])
+vHallArray = np.array([])
+
+output = open("IvsVh", "a")
+while True:
+	#### INPUT DATI DA ARDUINO
+	if stato == 0:
+		data = ser.readline().decode('utf-8').rstrip()
+		print (data)
+	if data == "CORRENTE" or stato == 3:
+		I = ser.readline().decode('utf-8').rstrip()
+		print ("corr " + I)
+		stato = 0
+	vArduino = open("vArduino"+ str(I) +".dat", "a")
+	vHall = open("vHall" + str(I) + ".dat", "a")
+	if data == "VARD":
+		stato = 1
+	if stato == 1:
+		data = ser.readline().decode('utf-8').rstrip() #12.6
+		print (data)
+		if data == "VHALL":
+			stato = 2
+		else:
+			vArduino.write(data + "\n")
+			vArduino.close()
+			for word in data.split():
+				vArdArray = np.append(vArdArray, float(word))
+	if stato == 2:
+		data = ser.readline().decode('utf-8').rstrip()
+		print (data)
+		if data == "CORRENTE":
+			stato = 3
+			mediaVarduino = np.mean(vArdArray)
+			devStdVard = np.std(vArdArray)
+			print("Vard: " + str(mediaVarduino) + "+/-" + str(devStdVard))
+			mediaVhall = np.mean(vHallArray)
+			devStdVh = np.std(vHallArray)
+			print("Vh: " + str(mediaVhall)+ "+/-" + str(devStdVh))
+			output.write(str(I) + " " + str(mediaVhall) + " " + str(mediaVarduino) + " " + str(devStdVard) + " " + str(devStdVh) + "\n")
+		elif data == "BREAK":
+			mediaVarduino = np.mean(vArdArray)
+			devStdVard = np.std(vArdArray)
+			print("Vard: " + str(mediaVarduino) + "+/-" + str(devStdVard))
+			mediaVhall = np.mean(vHallArray)
+			devStdVh = np.std(vHallArray)
+			print("Vh: " + str(mediaVhall)+ "+/-" + str(devStdVh))
+			output.write(str(I) + " " + str(mediaVhall) + " " + str(mediaVarduino) + " " + str(devStdVard) + " " + str(devStdVh) + "\n")
+			ser.close()
+			break
+		elif data == "VARD":
+			stato = 1
+		else:
+			vHall.write(data + "\n")
+			vHall.close()
+			for word in data.split():
+				vHallArray = np.append(vHallArray, float(word))
+
+'''
+stato = 0
+while True:
+	if ser.readline().decode('utf-8').rstrip() == "CORRENTE" and stato == 0: #corrente
+		I = ser.readline().decode('utf-8').rstrip() #0.00
+		vHall = open("vHall" + str(I) + ".dat", "a")
+		stato = 1
+	elif ser.readline().decode('utf-8').rstrip() == "VARD" and stato == 1:
+		vArduino = open("vArduino"+ str(I) +".dat", "a")
+		data = ser.readline().decode('utf-8').rstrip()
+		vArduino.write(data + "\n")
+		vArduino.close()
+	if ser.readline().decode('utf-8').rstrip() == "CORRENTE"
+'''
+#histo = ROOT.THD1("dist","title", 50, 0, 5)
---
+++ sketch.ino
@@ -0,0 +1,65 @@
+int main (void) {
+  init();
+  pinMode(10, OUTPUT);
+  pinMode(A0, INPUT); //v hall
+  pinMode(A4, INPUT); // v al generatore
+  //Serial3.begin(9600); // pin 15 RX e 14 TX
+  Serial.begin(9600);
+
+  int f =  20;
+  float V_ard_low[f], V_ard_high[f];
+  float V_hall_high[f], V_hall_low[f];
+  float dV_hall, dV_ard;
+  float incremento_corrente = 0.1;
+  float I_max = 0.2;
+  //Serial3.println("OP1 1 LF");
+  //Serial3.println("I1 0 LF");
+  //Serial3.println("V1 30 LF");
+
+  delay(500);
+  
+  for(float I=0; I <= I_max ; I+= incremento_corrente){
+    Serial.println("CORRENTE");
+    Serial.println(I);
+
+    for(int M = 0; M <= 2 ; M += 1){ // M misure di un "periodo"
+
+      for (int N = 0 ; N < f ; N += 1){ //N misure 5V
+        digitalWrite(10, HIGH);
+        V_ard_high[N] = map(analogRead(A0), 0, 1023, 0, 5);
+        V_hall_high[N] = map(analogRead(A4), 0, 1023, 0, 5);
+      }
+
+      for(int K=0 ; K < f ; K += 1){ //N misure 0V
+        digitalWrite(10, LOW);
+        V_ard_low[K] = map(analogRead(A0), 0, 1023, 0, 5);
+        V_hall_low[K] = map(analogRead(A4), 0, 1023, 0, 5);
+      }
+
+      Serial.println("VARD");
+      for ( int T = 0 ; T < f ; T++){
+        dV_ard = (V_ard_high[T]-V_ard_low[T]);
+        String vard = String(dV_ard) + " ";
+        Serial.print(vard);  // PRINTO N VALORI
+      }
+      Serial.println(" ");
+      delay(100);
+
+      Serial.println("VHALL");
+      for ( int T = 0 ; T < f ; T++){
+        dV_hall = (V_hall_high[T]-V_hall_low[T]);
+        String vhall = String(dV_hall) + " ";
+        Serial.print(vhall); //PRINTO N VALORI 
+      }
+      Serial.println(" ");
+      delay(100);
+    }
+    delay(100);
+    String corrente = "I1 " + String(I) + " LF";
+    //Serial3.println(corrente);
+  }
+  delay(100);
+  Serial.println("BREAK");
+  delay(100);
+  return 0;
+}
