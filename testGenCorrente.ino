0adcdc62b22da42d7e0b84cc6a4997d67ca86f0c
andreacosso <andrebog11@gmail.com>
2022-05-11 18:14:52 +0200

    JBOB

--- testGenCorrente.ino
+++ testGenCorrente.ino
@@ -7,5 +7,24 @@ int main(void){
   digitalWrite(10, HIGH);
   digitalWrite(11, LOW);
 
+  
+  /*
+  
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+
+  */
+
+
+ /*
+  UN ALTRA VERIFICA UTLIE è 
+
+  digitalWrite(10, LOW); 
+
+  CON IL GROUND DI ARDUINO 
+
+ */
+
+
   return 0;
 }
